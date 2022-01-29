6225f00158c5f7fdaabe19a53837b2eeb9980106
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2022-01-29 13:44:41 -0300

    Finish Requirements Installer CLI

--- config/cli.py
+++ config/cli.py
@@ -4,27 +4,69 @@ from os import system
 import os
 
 class Requirements: 
-    VERSION_REQUIREMENTS_CLI = "1.0.0"
+    __VERSION_REQUIREMENTS_CLI = "1.0.0"
+    __DEFAULT_REQUIREMENTS_PATH = ["./requirements.prod.txt", "./requirements.dev.txt", "./requirements.txt"]
 
     def __init__(self) -> None:
         self.__run()
 
     def __str__(self) -> str:
-        return f"Requirements Instance Object {self.VERSION_REQUIREMENTS_CLI}"
+        return f"Requirements Installer - {self.__VERSION_REQUIREMENTS_CLI}"
 
-    def __install_dependencies(self, python_interpreter="venv", file_name="requirements", env="d") -> bool: 
-        return True
+    def __is_path_reqquirements__(self, requirement_path: str) -> bool:
+        return os.path.isfile(requirement_path)
+
+    def __install_dependencies(self, python_interpreter: str = "venv", file_name="requirements", env="d") -> bool: 
+        if file_name == "requirements":
+            for requirement_path in self.__DEFAULT_REQUIREMENTS_PATH:
+                if self.__is_path_reqquirements__(requirement_path=requirement_path):
+                    file_name = requirement_path
+
+        if ((env == "p") and (".dev" in file_name)) or ((env == "d") and (".prod" in file_name)):
+            enviroment = "dev env" if env == "d" else "prod env"
+            current_env = "dev env" if "prod" in enviroment else "prod env"
+            print(f"ERROR: You passed {enviroment} and the env on project is {current_env}")
+
+        if python_interpreter == "venv":
+            try:
+                print("Installing requirements...")
+                system(f"pip install -r {file_name}")
+                
+                return True
+
+            except Exception as e:
+                print(f"ERROR: {e}")
+                return False
+
+        if python_interpreter != "venv":
+            aux = python_interpreter
+            print("Configuring the pip...")
+            
+            if os.path.isfile(aux.replace("python.exe", "Scripts\\pip.exe")):
+                python_interpreter = python_interpreter.replace("python.exe", "Scripts\\pip.exe")
+            else:
+                python_interpreter = python_interpreter.replace("\\python.exe", "\\pip.exe")
+
+            try:
+                print("Installing requirements...")
+                system(f"{python_interpreter} install -r {file_name}")
+                
+                return True
+
+            except Exception as e:
+                print(f"ERROR: {e}")
+                return False
 
     def __run(self):
         self.parser = ArgumentParser(
             prog="Requirements Installer",
             description="Automação para instalação dos requirements",
             epilog="Desenvolvido por: Pedro Augusto Barbosa Aparecido",
-            usage="%(prog)s [options]"
+            usage="%(prog)s [OPTIONS...] [VALUES...]"
 
         )
 
-        self.parser.version = self.VERSION_REQUIREMENTS_CLI
+        self.parser.version = self.__str__()
         self.parser.add_argument("-v", "--version", help="Show version of Requirements Installer.", action="version")
         self.parser.add_argument("-d", "--dev", help="Install dev dependencies on files XXXX.prod.txt.", action="store_true", default=False, required=False)
         self.parser.add_argument("-p", "--prod", help="Install prod dependencies on files XXXX.dev.txt.", action="store_true", default=False, required=False)
@@ -47,9 +89,11 @@ class Requirements:
                             print("-------------------")
                             print("\n\nValid Interpreter")
                         except Exception as e:
+                            args.interpreter = "venv"
                             print("\n\nInvalid Interpreter")
                             print(e)
                     else:
+                        args.interpreter = "venv"
                         print("Invalid Interpreter!! System using your venv or global python")
 
                 if args.prod:
