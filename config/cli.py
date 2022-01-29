be8313973703f96a46a65d2319e0a92db0009f99
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2022-01-29 09:29:31 -0300

    Create condition to install dependencies

--- config/cli.py
+++ config/cli.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 from os import system
 
+import os
+
 class Requirements: 
     VERSION_REQUIREMENTS_CLI = "1.0.0"
 
@@ -11,7 +13,7 @@ class Requirements:
         return f"Requirements Instance Object {self.VERSION_REQUIREMENTS_CLI}"
 
     def __install_dependencies(self, python_interpreter="venv", file_name="requirements", env="d") -> bool: 
-        ...
+        return True
 
     def __run(self):
         self.parser = ArgumentParser(
@@ -26,7 +28,7 @@ class Requirements:
         self.parser.add_argument("-v", "--version", help="Show version of Requirements Installer.", action="version")
         self.parser.add_argument("-d", "--dev", help="Install dev dependencies on files XXXX.prod.txt.", action="store_true", default=False, required=False)
         self.parser.add_argument("-p", "--prod", help="Install prod dependencies on files XXXX.dev.txt.", action="store_true", default=False, required=False)
-        self.parser.add_argument("-f", "--file", help="Especify the requirements filename or file path, default is '.' of project.", action="store", type=str, default=".", required=False)
+        self.parser.add_argument("-f", "--file", help="Especify the requirements filename or file path, default is 'requirements.[dev|prod].txt' on '.' of project.", action="store", type=str, default=".", required=False)
         self.parser.add_argument("-i", "--interpreter", help="Especify interpreter path if you don't use virtual enviroment, default is your venv.", action="store", type=str, default="venv", required=False)
 
         args = self.parser.parse_args()
@@ -35,6 +37,41 @@ class Requirements:
             try:
                 if not args.dev and not args.prod:
                     print("ERROR: Especify your enviroment please!!!\nOPTIONS: '-d|--dev', '-p|--prod'")
+
+                if args.interpreter != "venv":
+                    if os.path.isfile(args.interpreter):
+                        try:
+                            print("Testing interpreter")
+                            print("-------------------")
+                            system(f"{args.interpreter} -V")
+                            print("-------------------")
+                            print("\n\nValid Interpreter")
+                        except Exception as e:
+                            print("\n\nInvalid Interpreter")
+                            print(e)
+                    else:
+                        print("Invalid Interpreter!! System using your venv or global python")
+
+                if args.prod:
+                    if args.file == ".":
+                        if self.__install_dependencies(python_interpreter=args.interpreter, env="p"):
+                            print("Prod dependencies has been installed")
+                    elif os.path.isfile(args.file):
+                        if self.__install_dependencies(python_interpreter=args.interpreter, file_name=args.file, env="p"):
+                            print("Prod dependencies has been installed")
+                    else:
+                        print("ERROR: No such file or directory!!")
+                        
+
+                elif args.dev:
+                    if args.file == ".":
+                        if self.__install_dependencies(python_interpreter=args.interpreter):
+                            print("Dev dependencies has been installed")
+                    elif os.path.isfile(args.file):
+                        if self.__install_dependencies(python_interpreter=args.interpreter, file_name=args.file, env="d"):
+                            print("Dev dependencies has been installed")
+                    else:
+                        print("ERROR: No such file or directory!!")
 
             except:
                 self.parser.print_help()
